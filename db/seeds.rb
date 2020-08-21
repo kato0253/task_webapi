cf94f4dd45db73ad074fb618b7daa158554226b3
kato <user@MacBook-Air.local>
2020-08-21 20:43:12 +0900

    add:seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,32 @@
-10.times do |n|
 User.create!(
-             email: "sample#{n + 1}@example.com",
-             password: '00000000',
-             admin: false,
-            )
-end
+  name: "sample1",
+  email: "sample1@example.com",
+  password: '000000',
+  admin: true,
+)
 
-10.times do |n|
+  title  = "第0タイトル"
+  content = "第0コンテンツ"
+  Article.create!(
+    title: title,
+    content: content,
+    user_id: 1,
+)
+
 User.create!(
-             email: "sample#{n + 11}@example.com",
-             password: '00000000',
-             admin: true,
-            )
+  name: "sample2",
+  email: "sample2@example.com",
+  password: '000000',
+  admin: false,
+)
+
+19.times do |n|
+  title  = "第#{n+1}タイトル"
+  content = "第#{n+1}コンテンツ"
+  Article.create!(
+    title: title,
+    content: content,
+    user_id: 2,
+  )
+  
 end
