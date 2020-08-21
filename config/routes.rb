3d690bdb4ab7f8962f4226a8d1006a9bfee60745
kato <user@MacBook-Air.local>
2020-08-21 22:54:53 +0900

    add:admin画面ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'my_page', to: 'users/registrations#my_page', as: 'my_page'
   end
+  
   resources :favorites, only: [:create, :destroy]
 
   resources :articles do
@@ -28,9 +29,4 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  namespace 'api' do
-    namespace 'v1' do
-      resources :posts
-    end
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2020_08_21_081918) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.integer "image"
-    t.string "name", default: "", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
